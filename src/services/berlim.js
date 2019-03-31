7b34c56a5a00b7e72b18b160a049789a49b8dc86
Elias Sousa <humb.eliasj@gmail.com>
2019-03-31 12:39:08 -0300

    Add env vars

--- src/services/berlim.js
+++ src/services/berlim.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import { getToken } from "./auth";
 
 const berlim = axios.create({
-  baseURL: "http://localhost:4000/api"
+  baseURL: process.env.REACT_APP_API_URL
 });
 
 berlim.interceptors.request.use(async config => {
