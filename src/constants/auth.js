38482c9b1a4d62df9fec8b6cb7ad3e7a229b9a7a
Elias Sousa <humb.eliasj@gmail.com>
2019-03-31 17:27:54 -0300

    Auth js Refactor

--- src/constants/auth.js
+++
@@ -1,3 +0,0 @@
-export const TOKEN_KEY = "@berlim-token";
-export const USER_TYPE_KEY = "@berlim-user-type";
-export const USER_ID_KEY = "@berlim-user-id";
--- src/services/auth.js
+++ src/services/auth.js
@@ -1,32 +1,28 @@
 import decode from "jwt-decode";
-import { TOKEN_KEY, USER_TYPE_KEY, USER_ID_KEY } from "../constants/auth";
+
+const TOKEN_KEY = "@berlim-token";
 
 export const getToken = () => localStorage.getItem(TOKEN_KEY);
-export const isAdmin = () => localStorage.getItem(USER_TYPE_KEY) === "Admin";
+export const isAdmin = () => {
+  try {
+    const { type } = decode(getToken());
+    return type === "Admin";
+  } catch (err) {
+    return false;
+  }
+};
 export const isAuthenticated = () => {
   const token = getToken();
-
   return !!token && !isTokenExpired(token);
 };
 
-export const loginUser = token => {
-  const { type, sub } = decode(token);
-  localStorage.setItem(TOKEN_KEY, token);
-  localStorage.setItem(USER_TYPE_KEY, type);
-  localStorage.setItem(USER_ID_KEY, sub);
-};
-export const logoutUser = () => {
-  localStorage.removeItem(TOKEN_KEY);
-  localStorage.removeItem(USER_TYPE_KEY);
-  localStorage.removeItem(USER_ID_KEY);
-};
+export const loginUser = token => localStorage.setItem(TOKEN_KEY, token);
+export const logoutUser = () => localStorage.removeItem(TOKEN_KEY);
 
 const isTokenExpired = token => {
   try {
     const { exp } = decode(token);
-    return exp < Date.now() / 1000
-      return true;
-    } else return false;
+    return exp < Date.now() / 1000;
   } catch (err) {
     return false;
   }
